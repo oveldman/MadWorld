a26c05d167fb0fb4d938150cc8c018b1f7c8aa03
Oscar Veldman <oveldman@gmail.com>
2021-05-20 09:21:42 +0200

    remove scripts

--- migrate-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bash/sh
-dotnet ef migrations add InitialCreate
-dotnet ef migrations add InitialCreate --context AuthenticationContext
-dotnet ef database update
-dotnet ef database update --context AuthenticationContext
-dotnet ef migrations remove
