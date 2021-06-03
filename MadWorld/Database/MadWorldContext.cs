725aeba0c23b29f027e610ac5298ef33bdcde823
Oscar Veldman <oveldman@gmail.com>
2021-06-03 21:52:34 +0200

    Small dbset fix

--- MadWorld/Database/MadWorldContext.cs
+++ MadWorld/Database/MadWorldContext.cs
@@ -6,8 +6,8 @@ namespace Database
 {
     public class MadWorldContext : DbContext
     {
-        public virtual DbSet<Log> Logs { get; set; }
-        public virtual DbSet<Resume> Resumes { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Resume> Resumes { get; set; }
 
         public MadWorldContext(DbContextOptions<MadWorldContext> options) : base(options)
         {
