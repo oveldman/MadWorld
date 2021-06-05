9ccb527d5b14390d0f83aa0e89e6296fcda4b005
Oscar Veldman <oveldman@gmail.com>
2021-06-05 21:22:18 +0200

    Finish find page

---
+++ MadWorld/Shared/Models/PlanningPoker/PokerAccount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Website.Shared.Models.PlanningPoker
+{
+    public class PokerAccount
+    {
+        public int ID { get; set; }
+        [Required]
+        public string RoomName { get; set; }
+        [Required]
+        public string Username { get; set; }
+    }
+}
--- MadWorld/Website/Settings/LocalStorageNames.cs
+++ MadWorld/Website/Settings/LocalStorageNames.cs
@@ -4,5 +4,6 @@ namespace Website.Settings
     public static class LocalStorageNames
     {
         public static readonly string Login = "Login";
+        public static readonly string PlanningPokerAccount = "PokerAccount";
     }
 }
