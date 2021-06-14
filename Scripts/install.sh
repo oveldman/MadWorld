07b3b6402edfcec9dfad24146a50dd16661a8977
Oscar Veldman <oveldman@gmail.com>
2021-06-14 19:51:49 +0200

    Added Latest jaeger settings

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -40,16 +40,11 @@ docker run --name mad-world-db -e POSTGRES_PASSWORD=notmyrealpassword -e POSTGRE
 docker run --name auth-mad-world-db -e POSTGRES_PASSWORD=notmyrealpassword -e POSTGRES_DB=AuthenticationMadWorldDB -d -p 8081:5432 postgres
 
 # Jeager Docker
+# https://www.jaegertracing.io/docs/1.23/getting-started/
 docker run -d --name jaeger \
-  -e COLLECTOR_ZIPKIN_HOST_PORT=:9411 \
-  -p 5775:5775/udp \
+  -e QUERY_BASE_PATH=/jaeger \
   -p 6831:6831/udp \
-  -p 6832:6832/udp \
-  -p 5778:5778 \
   -p 16686:16686 \
-  -p 14268:14268 \
-  -p 14250:14250 \
-  -p 9411:9411 \
   jaegertracing/all-in-one:1.23
 
 # Libgdiplus
