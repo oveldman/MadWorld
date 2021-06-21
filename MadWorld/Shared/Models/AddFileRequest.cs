7a5ee88868f76727454f17bb22ee1c765142a0b6
Oscar Veldman <oveldman@gmail.com>
2021-06-21 21:47:08 +0200

    Add upload page

--- MadWorld/Shared/Models/AddFileRequest.cs
+++ MadWorld/Shared/Models/AddFileRequest.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Net.Http;
+
 namespace Website.Shared.Models
 {
     public class AddFileRequest
     {
+        public Guid? ID { get; set; }
+        public StreamContent Body { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
     }
 }
