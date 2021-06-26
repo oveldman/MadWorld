30b642f51340d66317bc0c7e7c30108596e6d88d
Oscar Veldman <oveldman@gmail.com>
2021-06-26 13:07:12 +0200

    Add cscharp compiler in website

---
+++ MadWorld/Website/Manager/EditorManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Website.Manager
+{
+    public static class EditorManager
+    {
+        public static string ResultToPrint { get; private set; } = string.Empty;
+
+        public static void Reset()
+        {
+            ResultToPrint = string.Empty;
+        }
+
+        public static void WriteLine(string showText)
+        {
+            ResultToPrint += showText + Environment.NewLine;
+        }
+    }
+}
