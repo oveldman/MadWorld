478f7f422fa7a41fa65a25ad1a4dea9ef9ffd8b2
Oscar Veldman <oveldman@gmail.com>
2021-07-02 18:39:53 +0200

    Add swagger to prod

--- MadWorld/API/Startup.cs
+++ MadWorld/API/Startup.cs
@@ -228,9 +228,10 @@ namespace API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {
