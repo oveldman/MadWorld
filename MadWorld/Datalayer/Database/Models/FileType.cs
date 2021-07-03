d0b0843cc8d87672aa5690edd98dbb052b89f07e
Oscar Veldman <oveldman@gmail.com>
2021-07-03 17:30:02 +0200

    File type update

--- MadWorld/Datalayer/Database/Models/FileType.cs
+++ MadWorld/Datalayer/Database/Models/FileType.cs
@@ -3,6 +3,9 @@ namespace Datalayer.Database.Models
 {
     public enum FileType
     {
-        Anonymous = 0
+        Anonymous = 0,
+        Admin = 1,
+        Private = 2,
+        Images = 3
     }
 }
