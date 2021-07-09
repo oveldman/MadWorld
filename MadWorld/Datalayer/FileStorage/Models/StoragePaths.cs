1181c0a4a5ceeee746dc87e76a1a55e273773d53
Oscar Veldman <oveldman@gmail.com>
2021-07-09 23:17:26 +0200

    update storagepaths

--- MadWorld/Datalayer/FileStorage/Models/StoragePaths.cs
+++ MadWorld/Datalayer/FileStorage/Models/StoragePaths.cs
@@ -1,8 +1,19 @@
 ﻿using System;
 namespace Datalayer.FileStorage.Models
 {
+    /// <summary>
+    /// Hardcoded file paths to find the file locations
+    /// </summary>
     public class StoragePaths
     {
-          public static string FreeFiles { get; set; } = "FreeDownload";
+        /// <summary>
+        /// Path to save html blog posts
+        /// </summary>
+        public static string BlogFiles { get; private set; } = "Blogs";
+
+        /// <summary>
+        /// Path to save free download to anonymous users
+        /// </summary>
+        public static string FreeFiles { get; private set; } = "FreeDownload";
     }
 }
